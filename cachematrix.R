ba320181d9f79b0fdb4be864e49f58560a460879
Eli Jara <68289620+enjara@users.noreply.github.com>
2020-08-08 19:29:21 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this caches the inverse matrix
 
-## Write a short comment describing this function
+makecachematrix <- function(x = matrix()) {
+  m <- NULL
+       get <- function() x
+       setinv <- function(Ginv) m <<- Ginv
+       list(get = get,
+            setinv = Ginv)
+   }
 
-makeCacheMatrix <- function(x = matrix()) {
 
-}
-
-
-## Write a short comment describing this function
+## This solves the previously cached inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+       m <- x[Ginv(x)]
+       if(!is.null(m)) {
+           message("this is not working")
+           return(m)
+       }
+       data <- x[get()]
+       m <- Ginv(data, ...)
+       m
+  }
